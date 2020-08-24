f2febb1377a90f72e9cf8d1fb9e9501f61753a8c
lixiang <2646304485@qq.com>
2020-08-24 17:43:28 +0800

    不知道为什么文件serve无效

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue' //先导入vue
+import Router from 'vue-router' //再导入vue-router 
 
 import Home from './pages/home.vue'
 
@@ -15,67 +15,64 @@ import OrderList from './pages/orderList.vue'
 import OrderPay from './pages/orderPay.vue'
 import aliPay from './pages/aliPay.vue'
 
- 
-Vue.use(Router);
+
+Vue.use(Router); //Vue.use加载插件
 
 export default new Router({
-    routes: [
-        {
-        path:'/',
-        name:'home',
-        component:Home,
-        redirect:'/index',
-        children:[
-            {
-                path:'/index',
-                name:'index',
-                component:Index,  
-            },
-            {
-                path:'/product/:id',
-                name:'product',
-                component:Product,  
-            },
-            {
-                path:'/detail/:id',
-                name:'detail',
-                component:Detail,  
-            },
-        ]
+    routes: [{
+            path: '/',
+            name: 'home',
+            component: Home,
+            redirect: '/index',
+            children: [{
+                    path: '/index',
+                    name: 'index',
+                    component: Index,
+                },
+                {
+                    path: '/product/:id',
+                    name: 'product',
+                    component: Product,
+                },
+                {
+                    path: '/detail/:id',
+                    name: 'detail',
+                    component: Detail,
+                },
+            ]
         },
         {
-            path:'/cart',
-            name:'cart',
-            component:Cart,  
+            path: '/cart',
+            name: 'cart',
+            component: Cart,
         },
         {
-            path:'/order',
-            name:'order',
-            component:Order,
-            children:[
-                {
-                    path:'list',
-                    name:'order-list',
-                    component:OrderList,
+            path: '/order',
+            name: 'order',
+            component: Order,
+            children: [{
+                    path: 'list',
+                    name: 'order-list',
+                    component: OrderList,
                 },
                 {
-                    path:'confirm',
-                    name:'order-confirm',
-                    component:OrderConfirm,
+                    path: 'confirm',
+                    name: 'order-confirm',
+                    component: OrderConfirm,
                 },
                 {
-                    path:'pay',
-                    name:'order-pay',
-                    component:OrderPay,
+                    path: 'pay',
+                    name: 'order-pay',
+                    component: OrderPay,
                 },
                 {
-                    path:'alipay',
-                    name:'alipay',
-                    component:aliPay,
+                    path: 'alipay',
+                    name: 'alipay',
+                    component: aliPay,
                 }
 
             ]
         }
-          
+
     ]
 });
--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,22 @@
-module.exports={
-    devServer:{
-        port:8085,
-        host:"localhost",
-        https:false,
-        open:true,
-        proxy:{
-            '/api':{
-                target:"http://mengxuegu.com:7300/mock/5ee1078aaed99d0634618f25",
-                changeOrigin:true,
-                pathRewrite:{
-                    '/api':'',
-                } 
-            }
-
-        }
+module.exports = {
+    devServer: {
+        port: 8085,
+        host: "localhost",
+        https: false,
+        open: true,
+        // proxy: {
+        //     '/api': {
+        //         target: "http://mengxuegu.com:7300/mock/5ee1078aaed99d0634618f25",
+        //         changeOrigin: true,
+        //         pathRewrite: {
+        //             '/api': '',
+        //         }
+        //     }
+        // },
+        lintOnSave: false, //关闭格式检查
+        // outputDir:"dist2",   //存放 打包文件的目录 默认 dist
+        // assetsDir:"assets",   // 静态资源（js，css，img）打包存放路径（相对outputDir 指定的路径）
+        // indexPath:"out/index.html",   // index.html  主页面打包后存放的目录（相对outputDir 指定的路径）
+        productionSourceMap: false, //打包时不会生成  .map 文件  加快打包速度
     }
 }
